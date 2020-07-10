64236793f00b72f294e2f8bcd64bd9bf6c550986
Jerry <szy0syz@gmail.com>
2020-07-10 10:27:46 +0800

    Init flutter_sqlite_todos

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # flutter-demos
-more demos
---
+++ flutter_sqlite_todos/flutter_sqlite_todos/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqlite_todos/screens/todo_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
