7649c3ae05b635766d1727d572b61ccce758f2fb
Jerry <szy0syz@gmail.com>
2020-07-10 11:34:12 +0800

    Update flutter-todos list-screen

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flutter-demos
+
+## Notes
+
+- 代码片段快捷前缀：`stl` - `Flutter stateless widget` or `Flutter stateful widget`
+- vscode-flutter 套壳快捷键：`cmd + .`，好用到耐不住啊
--- flutter_sqlite_todos/lib/screens/todo_list_screen.dart
+++ flutter_sqlite_todos/lib/screens/todo_list_screen.dart
@@ -6,8 +6,70 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
+  Widget _buildTask(int index) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('Task Title'),
+            subtitle: Text('Oct 2, 2020 * High'),
+            trailing: Checkbox(
+                value: true,
+                activeColor: Theme.of(context).primaryColor,
+                onChanged: (value) {
+                  print(value);
+                }),
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Navigate to add task screen');
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: 80.0),
+          itemCount: 10,
+          itemBuilder: (BuildContext content, int index) {
+            if (index == 0) {
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'My tasks',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Text(
+                      '1 of 10',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+            return _buildTask(index);
+          }),
+    );
   }
 }
