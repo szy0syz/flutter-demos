9d59a23ea888f5ec1d73535a413a5c643c40239c
szy0syz <szy0syz@gmail.com>
2020-07-11 13:25:02 +0800

    Update BMI-Calc

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 - flutter_clima
 - flutter_flash_chat
 
-![01](preivew/todo_list.gif)
+![01](preview/todo_list.gif)
 
 ## Notes
 
@@ -18,7 +18,25 @@
 - 如果不想让格式化器换行代码，就加一个 `,` 在末尾
 - 容器宽度或高度占满全屏怎么办 -> `width: double.infinity`
 - `Flutter` 中怎么更新 `ListView` ？
-  - 2
+- 关于类的构造函数的参数问题
+
+```dart
+/// 此时 colour 是可选参数，且不能以 _ 开头
+class ReusableCard extends StatelessWidget {
+  ReusableCard({this.colour});
+
+  /// 此时就为必填参数了
+  /// ReusableCard({@Required this.colour});
+}
+```
+
+- Instance Variable = Field = Property
+- 布局技巧
+  - 最外层 `Column` 行组件
+  - 每行先来一个 `Row` 列组件
+  - 最后每行的里，再包一个 `Expanded` 撑开组件，因为中间那个沾满整行，如果两个就50%排布
+
+![layout1](preview/layout1.png)
 
 ```dart
 /// 原来是这样初始化 state
---
+++ flutter-bmi-calculator/lib/screens/input_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
