d77950f922cfb5810dad5da1b1ed5734decf3915
szy0syz <szy0syz@gmail.com>
2020-07-24 23:13:33 +0800

    Finish flutter_rotated_menu

--- flutter_rotated_menu/lib/main.dart
+++ flutter_rotated_menu/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(MyApp());
@@ -24,7 +25,25 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    animation = CurvedAnimation(parent: controller, curve: Curves.bounceOut);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -33,7 +52,15 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Stack(
         children: <Widget>[
           buildBody(),
-          buildMenu(size),
+          AnimatedBuilder(
+              animation: controller,
+              builder: (context, snapshot) {
+                return Transform.rotate(
+                  angle: -math.pi / 2 * animation.value,
+                  origin: Offset(-size.width / 2 + 40, -size.height / 2 + 60),
+                  child: buildMenu(size),
+                );
+              }),
         ],
       ),
     );
@@ -42,9 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget buildBody() {
     return ListView(
       children: <Widget>[
-        SizedBox(
-          height: 100,
-        ),
+        SizedBox(height: 100),
         Image.asset('images/content_1.png'),
         Image.asset('images/content_2.png'),
       ],
@@ -53,29 +78,58 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget buildMenu(Size size) {
     return Container(
+      padding: EdgeInsets.only(left: 10),
       color: lightColor,
       width: size.width,
       height: size.height,
-      child: Column(
+      child: Stack(
         children: <Widget>[
-          SizedBox(height: 50),
-          Icon(Icons.menu, color: Colors.white, size: 30),
-          SizedBox(height: 40),
-          buildItem('images/ic_profile.png', "PROFILE"),
-          SizedBox(
-            height: 20,
-          ),
-          buildItem('images/ic_feed.png', "FEED"),
-          SizedBox(
-            height: 20,
-          ),
-          buildItem('images/ic_activity.png', "ACTIVITY", isSelected: true),
-          SizedBox(
-            height: 100,
-          ),
-          buildItem('images/ic_settings.png', "SETTINGS"),
+          buildMenuList(),
+          buildSelectedText(size),
         ],
       ),
+    );
+  }
+
+  Column buildMenuList() {
+    return Column(
+      /// 在一行的最左边
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 50),
+        InkWell(
+          onTap: () {
+            if (controller.isCompleted) {
+              controller.reverse();
+              animation = CurvedAnimation(
+                parent: controller,
+                curve: Curves.bounceIn,
+              );
+            } else {
+              controller.forward();
+              animation = CurvedAnimation(
+                parent: controller,
+                curve: Curves.bounceOut,
+              );
+            }
+          },
+          child: Icon(Icons.menu, color: Colors.white, size: 30),
+        ),
+        SizedBox(height: 40),
+        buildItem('images/ic_profile.png', "PROFILE"),
+        SizedBox(
+          height: 20,
+        ),
+        buildItem('images/ic_feed.png', "FEED"),
+        SizedBox(
+          height: 20,
+        ),
+        buildItem('images/ic_activity.png', "ACTIVITY", isSelected: true),
+        SizedBox(
+          height: 100,
+        ),
+        buildItem('images/ic_settings.png', "SETTINGS"),
+      ],
     );
   }
 
@@ -102,6 +156,23 @@ class _MyHomePageState extends State<MyHomePage> {
               fontSize: 25),
         )
       ],
+    );
+  }
+
+  Widget buildSelectedText(Size size) {
+    return Positioned(
+      top: size.width / 2 - 40,
+      child: Opacity(
+        opacity: animation.value,
+        child: RotatedBox(
+          quarterTurns: 1,
+          child: Text(
+            "ACTICITY",
+            style: TextStyle(
+                fontFamily: 'IntroRust', color: Colors.white, fontSize: 25),
+          ),
+        ),
+      ),
     );
   }
 }
