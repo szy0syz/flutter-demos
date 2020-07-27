5f032fe169f2a084e1a98a0d5cb097b45bb4c599
szy0syz <szy0syz@gmail.com>
2020-07-27 23:25:55 +0800

    Update Flutter_Calc

--- flutter_calculator/lib/main.dart
+++ flutter_calculator/lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calculator/widgets/CalcButton.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(CalcApp());
 }
 
-class CalcApp extends StatelessWidget {
-  // This widget is the root of your application.
+class CalcApp extends StatefulWidget {
+  CalcApp({Key key}) : super(key: key);
+  @override
+  CalcAppState createState() => CalcAppState();
+}
+
+class CalcAppState extends State<CalcApp> {
+  String _history = '';
+  String _expression = '';
+
+  void numClick(String text) {
+    setState(() {
+      _expression += text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,6 +32,29 @@ class CalcApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(right: 12),
+              child: Text(
+                _history,
+                style: GoogleFonts.rubik(
+                  textStyle: TextStyle(fontSize: 24),
+                  color: Color(0xff545f61),
+                ),
+              ),
+              alignment: Alignment(1, 1),
+            ),
+            Container(
+              padding: EdgeInsets.all(12),
+              child: Text(
+                _expression,
+                style: GoogleFonts.rubik(
+                  textStyle: TextStyle(fontSize: 48),
+                  color: Colors.white,
+                ),
+              ),
+              alignment: Alignment(1, 1),
+            ),
+            SizedBox(height: 40),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
@@ -24,20 +62,24 @@ class CalcApp extends StatelessWidget {
                   text: 'AC',
                   fillColor: 0xFF6C807F,
                   textSize: 20,
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: 'C',
                   fillColor: 0xFF6C807F,
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '%',
                   fillColor: 0xFFFFFFFF,
                   textColor: 0xFF65BDAC,
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '/',
                   fillColor: 0xFFFFFFFF,
                   textColor: 0xFF65BDAC,
+                  callback: numClick,
                 ),
               ],
             ),
@@ -46,18 +88,22 @@ class CalcApp extends StatelessWidget {
               children: <Widget>[
                 CalcButton(
                   text: '7',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '8',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '9',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '*',
                   fillColor: 0xFFFFFFFF,
                   textColor: 0xFF65BDAC,
                   textSize: 24,
+                  callback: numClick,
                 ),
               ],
             ),
@@ -66,18 +112,22 @@ class CalcApp extends StatelessWidget {
               children: <Widget>[
                 CalcButton(
                   text: '4',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '5',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '6',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '-',
                   fillColor: 0xFFFFFFFF,
                   textColor: 0xFF65BDAC,
                   textSize: 38,
+                  callback: numClick,
                 ),
               ],
             ),
@@ -86,18 +136,22 @@ class CalcApp extends StatelessWidget {
               children: <Widget>[
                 CalcButton(
                   text: '1',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '2',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '3',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '+',
                   fillColor: 0xFFFFFFFF,
                   textColor: 0xFF65BDAC,
                   textSize: 30,
+                  callback: numClick,
                 ),
               ],
             ),
@@ -106,18 +160,22 @@ class CalcApp extends StatelessWidget {
               children: <Widget>[
                 CalcButton(
                   text: '.',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '0',
+                  callback: numClick,
                 ),
                 CalcButton(
                   text: '00',
+                  callback: numClick,
                   textSize: 26,
                 ),
                 CalcButton(
                   text: '=',
                   fillColor: 0xFFFFFFFF,
                   textColor: 0xFF65BDAC,
+                  callback: numClick,
                 ),
               ],
             ),
