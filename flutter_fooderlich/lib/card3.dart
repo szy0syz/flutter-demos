199359820112324eb6e43082bce556d02d30c6f5
szy0syz <szy0syz@gmail.com>
2021-12-01 18:08:44 +0800

    update: fooderlich - card3

---
+++ flutter_fooderlich/lib/card3.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+class Card3 extends StatelessWidget {
+  const Card3({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag2.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Stack(
+          children: [
+            // TODO 5: add dark overlay BoxDecoration
+            // TODO 6: Add Container, Column, Icon and Text
+            // TODO 7: Add Center widget with Chip widget children
+          ],
+        ),
+      ),
+    );
+  }
+}
