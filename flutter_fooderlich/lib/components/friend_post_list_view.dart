62d363031d7f08671bbba5898ccf5357c37ae20f
szy0syz <szy0syz@gmail.com>
2021-12-02 15:02:13 +0800

    update: nested ListView

---
+++ flutter_fooderlich/lib/components/friend_post_list_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/models/post.dart';
+
+import 'friend_post_tile.dart';
+
+class FriendPostListView extends StatelessWidget {
+  final List<Post> friendPosts;
+
+  const FriendPostListView({Key? key, required this.friendPosts})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Social Chefs ! ', style: Theme.of(context).textTheme.headline1),
+          const SizedBox(height: 12),
+          // TODO: Add PostListView here
+          const SizedBox(height: 16),
+          ListView.separated(
+            primary: false,
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            itemCount: friendPosts.length,
+            itemBuilder: (context, index) {
+              final post = friendPosts[index];
+              return FriendPostTile(post: post);
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(height: 16);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- flutter_fooderlich/lib/screens/explore_screen.dart
+++ flutter_fooderlich/lib/screens/explore_screen.dart
@@ -19,11 +19,7 @@ class ExploreScreen extends StatelessWidget {
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
               const SizedBox(height: 16),
-              // TODO: Replace this with FriendPostListView
-              Container(
-                height: 400,
-                color: Colors.green,
-              ),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
             ],
           );
         } else {
