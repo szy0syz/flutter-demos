854964914ffa2fbde9566de9fea0d403df6825c2
szy0syz <szy0syz@gmail.com>
2021-12-02 10:35:05 +0800

    update: fooderlish

--- flutter_fooderlich/lib/home.dart
+++ flutter_fooderlich/lib/home.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/material.dart';
-import 'card2.dart';
-import 'card3.dart';
-import 'card1.dart';
 
-// 1
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  // 7
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
 
-  // 8
   static List<Widget> pages = <Widget>[
-    const Card1(),
-    const Card2(),
-    const Card3(),
+    // TODO: Replace with ExploreScreen
+    Container(color: Colors.white),
+    // TODO: Replace with RecipesScreen
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
   ];
 
-  // 9
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -34,27 +30,26 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text(
           'Fooderlich',
-          // 2
           style: Theme.of(context).textTheme.headline6,
         ),
       ),
       body: pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
         items: <BottomNavigationBarItem>[
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card1',
+            icon: Icon(Icons.explore),
+            label: 'Explore',
           ),
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card2',
+            icon: Icon(Icons.book),
+            label: 'Recipes',
           ),
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card3',
+            icon: Icon(Icons.list),
+            label: 'To Buy',
           ),
         ],
       ),
