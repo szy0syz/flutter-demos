0265b69bc5a6aa1662c9777c857237246e59e197
szy0syz <szy0syz@gmail.com>
2021-12-02 13:38:59 +0800

    fix: fooderlich修复构建失败问题

---
+++ flutter_fooderlich1/lib/components/today_recipe_list_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../models/explore_recipe.dart';
+import 'card1.dart';
+import 'card2.dart';
+import 'card3.dart';
+
+class TodayRecipeListView extends StatelessWidget {
+  final List<ExploreRecipe> recipes;
+
+  const TodayRecipeListView({Key? key, required this.recipes})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recipes of the Day 🍳',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          const SizedBox(height: 16),
+          Container(
+            height: 400,
+            color: Colors.transparent,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: recipes.length,
+              itemBuilder: (context, index) {
+                final recipe = recipes[index];
+                return _buildCard(recipe);
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 16);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(ExploreRecipe recipe) {
+    if (recipe.cardType == RecipeCardType.card1) {
+      return Card1(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card2) {
+      return Card2(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card3) {
+      return Card3(recipe: recipe);
+    } else {
+      return Container();
+      // throw Exception('This card doesn\'t exist yet');
+    }
+  }
+}
