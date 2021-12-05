3864048fcd9766a363ad4cde1c2fa92392807e84
szy0syz <szy0syz@gmail.com>
2021-12-05 23:06:25 +0800

    update

--- flutter_fooderlich/lib/components/grocery_tile.dart
+++ flutter_fooderlich/lib/components/grocery_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/models/grocery_item.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class GroceryTile extends StatelessWidget {
   final GroceryItem item;
@@ -22,8 +23,40 @@ class GroceryTile extends StatelessWidget {
     // TODO 21: Change this Widget
     return Container(
       height: 100.0,
-      // TODO 20: Replace this color
-      color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(width: 5.0, color: item.color),
+          const SizedBox(width: 16.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                item.name,
+                style: GoogleFonts.lato(
+                    decoration: textDecoration,
+                    fontSize: 21.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 4.0),
+              buildDate(),
+              const SizedBox(height: 4.0),
+              buildImportance(),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                item.quantity.toString(),
+                style: GoogleFonts.lato(
+                    decoration: textDecoration, fontSize: 21.0),
+              ),
+              buildCheckbox()
+            ],
+          )
+        ],
+      ),
     );
   }
 
@@ -48,6 +81,20 @@ class GroceryTile extends StatelessWidget {
     }
   }
 
-  // TODO: Add buildDate()
-  // TODO: Add buildCheckbox()
+  Widget buildDate() {
+    final dateFormatter = DateFormat("MMMM dd h:mm a");
+    final dateString = dateFormatter.format(item.date);
+
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
+    );
+  }
+
+  Widget buildCheckbox() {
+    return Checkbox(
+      value: item.isComplete,
+      onChanged: onComplete,
+    );
+  }
 }
