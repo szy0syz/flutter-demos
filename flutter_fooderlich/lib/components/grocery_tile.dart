70620e625ad1810d2f497b513a31233b2c5ef100
szy0syz <szy0syz@gmail.com>
2021-12-06 20:55:35 +0800

    fix: grocery_tile

--- flutter_fooderlich/lib/components/grocery_tile.dart
+++ flutter_fooderlich/lib/components/grocery_tile.dart
@@ -20,43 +20,57 @@ class GroceryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            Container(width: 5.0, color: item.color),
-            const SizedBox(width: 16.0),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  item.name,
-                  style: GoogleFonts.lato(
+    return SizedBox(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // 1
+          Row(
+            children: [
+              // 2
+              Container(width: 5.0, color: item.color),
+              // 3
+              const SizedBox(width: 16.0),
+              // 4
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 5
+                  Text(
+                    item.name,
+                    style: GoogleFonts.lato(
                       decoration: textDecoration,
                       fontSize: 21.0,
-                      fontWeight: FontWeight.bold),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  buildDate(),
+                  const SizedBox(height: 4.0),
+                  buildImportance(),
+                ],
+              ),
+            ],
+          ),
+          // 6
+          Row(
+            children: [
+              // 7
+              Text(
+                item.quantity.toString(),
+                style: GoogleFonts.lato(
+                  decoration: textDecoration,
+                  fontSize: 21,
                 ),
-                const SizedBox(height: 4.0),
-                buildDate(),
-                const SizedBox(height: 4.0),
-                buildImportance(),
-              ],
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Text(
-              item.quantity.toString(),
-              style:
-                  GoogleFonts.lato(decoration: textDecoration, fontSize: 21.0),
-            ),
-            buildCheckbox()
-          ],
-        )
-      ],
+              ),
+              // 8
+              buildCheckbox()
+            ],
+          )
+        ],
+      ),
     );
   }
 
